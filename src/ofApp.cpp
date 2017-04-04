52dc71970e89859dd8af6971d8daac621dfd0263
KirRupteD <randy_cornelius@live.com>
2017-04-04 19:15:49 -0400

    Random numbers generated

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,10 +1,20 @@
 #include "ofApp.h"
+#include <random> 
 
 //--------------------------------------------------------------
 void ofApp::setup(){
 jewel.load("diamond.png");// image to use (must be stored in "random/bin/data" folder)
-X = {500, 1000, 30, 100, 300, 200, 750, 380 };// set of x-coordinates for jewel
-Y = {630, 400, 900,40, 200, 0, 90, 30}; //set of y-coordinates for jewel
+//X = {500, 1000, 30, 100, 300, 200, 750, 380 };// set of x-coordinates for jewel
+//Y = {630, 400, 900,40, 200, 0, 90, 30}; //set of y-coordinates for jewel
+std::random_device Jewel;
+std::mt19937 randomEngine(Jewel());
+std::uniform_int_distribution<>range1(0,1200);
+std::uniform_int_distribution<>range2(0,970);
+
+for(int i=1;i<+20;i++){
+	X.push_back(range1(randomEngine));
+	Y.push_back(range2(randomEngine));
+}
 }
 
 //--------------------------------------------------------------
